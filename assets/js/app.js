486ee8cb8bf733e9e698b3c4f8a61fa1f9e62270
Ameen Alalimi <ameenalalimi.dev@gmail.com>
2025-12-01 15:15:16 +0300

    completing sections

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,32 +22,3 @@ function updateToggleIcon() {
     toggle.classList.add("fa-moon");
   }
 }
-
-// Mobile menu toggle
-const mobileMenuToggle = document.getElementById("mobileMenuToggle");
-const mobileMenu = document.getElementById("mobileMenu");
-
-if (mobileMenuToggle && mobileMenu) {
-  mobileMenuToggle.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-    
-    // Toggle icon between bars and times
-    if (mobileMenu.classList.contains("hidden")) {
-      mobileMenuToggle.classList.remove("fa-times");
-      mobileMenuToggle.classList.add("fa-bars");
-    } else {
-      mobileMenuToggle.classList.remove("fa-bars");
-      mobileMenuToggle.classList.add("fa-times");
-    }
-  });
-  
-  // Close menu when clicking on a link
-  const mobileMenuLinks = mobileMenu.querySelectorAll("a");
-  mobileMenuLinks.forEach(link => {
-    link.addEventListener("click", () => {
-      mobileMenu.classList.add("hidden");
-      mobileMenuToggle.classList.remove("fa-times");
-      mobileMenuToggle.classList.add("fa-bars");
-    });
-  });
-}
