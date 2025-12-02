aa357fb2da91652c93ce304390131d6df7cc01be
Ameen Alalimi <ameenalalimi.dev@gmail.com>
2025-12-02 23:57:44 +0300

    changing the home section style

--- assets/js/app.js
+++ assets/js/app.js
@@ -48,3 +48,52 @@ contactForm.addEventListener("submit", function (e) {
     }
   );
 });
+
+const texts = [
+  "مطور مواقع",
+  "مطور تطبيقات موبايل",
+  "بناء انظمة محاسبية",
+  "استشارات تقنية",
+];
+let textIndex = 0; // النص الحالي
+let charIndex = 0; // الحرف الحالي
+let typingSpeed = 150; // سرعة الكتابة
+let deletingSpeed = 90; // سرعة المسح
+let pauseTime = 1500; // بقاء النص عند اكتماله
+
+function typeWriter() {
+  const element = document.getElementById("typing-text");
+  const currentText = texts[textIndex];
+
+  if (charIndex <= currentText.length && charIndex >= 0) {
+    element.textContent = currentText.substring(0, charIndex);
+  }
+
+  if (charIndex === currentText.length) {
+    // بعد اكتمال النص الانتظار ثم البدء في المسح
+    setTimeout(() => {
+      charIndex--;
+      typeWriterLoop();
+    }, pauseTime);
+  } else {
+    charIndex++;
+    setTimeout(typeWriter, typingSpeed);
+  }
+}
+
+function typeWriterLoop() {
+  const element = document.getElementById("typing-text");
+  const currentText = texts[textIndex];
+
+  if (charIndex >= 0) {
+    element.textContent = currentText.substring(0, charIndex);
+    charIndex--;
+    setTimeout(typeWriterLoop, deletingSpeed);
+  } else {
+    // الانتقال للنص التالي بعد المسح
+    textIndex = (textIndex + 1) % texts.length;
+    setTimeout(typeWriter, 700); // توقف بسيط قبل النص الجديد
+  }
+}
+// بدء الحركة 
+typeWriter();
